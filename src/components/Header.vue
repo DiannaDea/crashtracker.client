fd175a2c88709bb54336f1dcc0b4e9221a44ac04
Diana Baburina <diana.baburina@code-care.pro>
2019-04-24 00:07:28 +0300

    toggle buttons in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,13 +7,19 @@
       :defaultSelectedKeys="['1']"
       :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">
+      <a-menu-item key="1" v-if="!token">
+        <router-link to="/sign-in"><a-icon type="login" />Sign in</router-link>
+      </a-menu-item>
+      <a-menu-item key="2" v-if="!token">
+        <router-link to="/sign-up"><a-icon type="plus-circle" />Sign up</router-link>
+      </a-menu-item>
+      <a-menu-item key="3" v-if="token">
         <router-link to="/devices"><a-icon type="tool" />My devices</router-link>
       </a-menu-item>
-      <a-menu-item key="2">
+      <a-menu-item key="4" v-if="token">
         <router-link to="/add-device"><a-icon type="plus-circle" />Add device</router-link>
       </a-menu-item>
-      <a-menu-item key="3" @click="logout">
+      <a-menu-item key="5" @click="logout" v-if="token">
         <a-icon type="logout" />Logout
       </a-menu-item>
     </a-menu>
@@ -26,6 +32,11 @@ import * as actionTypes from '../store/action-types';
 
 export default {
   name: 'Header',
+  computed: {
+    ...mapState({
+      token: state => state.auth.token,
+    }),
+  },
   methods: {
     ...mapActions('auth', {
       signOut: actionTypes.SIGN_OUT,
